6f73e34f0d064daab12fcfe3ee8aeb89c5c9d1c8
ghnmqdtg <ghnmqdtg@gmail.com>
2024-04-18 14:49:23 +0800

    Feat: Add feature to save evaluation results into CSV file
    Fix

--- for_test.py
+++ for_test.py
@@ -12,6 +12,44 @@ from scipy.io.wavfile import write as swrite
 
 from utils.stft import STFTMag
 
+import csv
+import time
+
+
+def save_results_to_csv(results, filename="results.csv"):
+    # Check if file exists. If not, create it and write headers
+    file_exists = os.path.isfile(filename)
+    with open(filename, mode="a", newline="") as file:
+        writer = csv.writer(file)
+        if not file_exists:
+            writer.writerow(
+                [
+                    "Sample Rate",
+                    "SNR (mean)",
+                    "SNR (std)",
+                    "Base SNR (mean)",
+                    "Base SNR (std)",
+                    "LSD (mean)",
+                    "LSD (std)",
+                    "Base LSD (mean)",
+                    "Base LSD (std)",
+                    "LSD HF (mean)",
+                    "LSD HF (std)",
+                    "Base LSD HF (mean)",
+                    "Base LSD HF (std)",
+                    "LSD LF (mean)",
+                    "LSD LF (std)",
+                    "Base LSD LF (mean)",
+                    "Base LSD LF (std)",
+                    "RTF (mean)",
+                    "RTF (std)",
+                    "RTF Reciprocal (mean)",
+                    "RTF Reciprocal (std)",
+                ]
+            )
+
+        writer.writerow(results)
+
 
 def test(args):
     def cal_snr(pred, target):
@@ -36,7 +74,11 @@ def test(args):
 
     hparams = OC.load("hparameter.yaml")
     hparams.save = args.save or False
-    model = NuWave2(hparams, False).cuda()
+    if args.cuda:
+        model = NuWave2(hparams, False).cuda()
+    else:
+        model = NuWave2(hparams, False)
+
     if args.ema:
         ckpt_path = glob(
             os.path.join(hparams.log.checkpoint_dir, f"*_epoch={args.resume_from}_EMA")
@@ -65,17 +107,27 @@ def test(args):
         base_lsd_hf_list = []
         lsd_lf_list = []
         base_lsd_lf_list = []
+        # RTF: Real Time Factor
+        rtf_list = []
         t = model.test_dataloader(sr=args.sr)
         hf = int(1025 * (args.sr / hparams.audio.sampling_rate))
-        print(len(t))
-        for j, batch in tqdm(enumerate(t)):
+        for j, batch in enumerate(tqdm(t)):
             wav, wav_l, band = batch
-            # wav = wav.cuda()
-            wav_l = wav_l.cuda()
-            band = band.cuda()
+            if args.cuda:
+                # wav = wav.cuda()
+                wav_l = wav_l.cuda()
+                band = band.cuda()
+            else:
+                # wav = wav
+                wav_l = wav_l
+                band = band
+
+            start_time = time.time()
             wav_up, *_ = model.inference(
                 wav_l, band, 8, eval(hparams.dpm.infer_schedule)
             )
+            run_time = time.time() - start_time
+            rtf_list.append(run_time / wav_l.size(1) * hparams.audio.sampling_rate)
 
             wav_up = wav_up.cpu().detach()
             wav_l = wav_l.cpu().detach()
@@ -90,6 +142,7 @@ def test(args):
             base_lsd_hf_list.append(base_lsd_hf_j)
             lsd_lf_list.append(lsd_lf_j)
             base_lsd_lf_list.append(base_lsd_lf_j)
+
             if args.save and i == 0:
                 swrite(
                     f"{hparams.log.test_result_dir}/{args.sr}/test_{j}_up.wav",
@@ -115,6 +168,8 @@ def test(args):
         base_lsd_hf = torch.stack(base_lsd_hf_list, dim=0).mean()
         lsd_lf = torch.stack(lsd_lf_list, dim=0).mean()
         base_lsd_lf = torch.stack(base_lsd_lf_list, dim=0).mean()
+        rtf = torch.tensor(rtf_list).mean()
+        rtf_reciprocal = 1 / rtf
         dict = {
             "snr": snr.item(),
             "base_snr": base_snr.item(),
@@ -124,6 +179,8 @@ def test(args):
             "base_lsd_hf": base_lsd_hf.item(),
             "lsd_lf": lsd_lf.item(),
             "base_lsd_lf": base_lsd_lf.item(),
+            "rtf": rtf.item(),
+            "rtf_reciprocal": rtf_reciprocal.item(),
         }
         results.append(
             torch.stack(
@@ -136,14 +193,30 @@ def test(args):
                     base_lsd_hf,
                     lsd_lf,
                     base_lsd_lf,
+                    rtf,
+                    rtf_reciprocal,
                 ],
                 dim=0,
             ).unsqueeze(-1)
         )
         print(dict)
+
+    # Save results to csv
+    # Loop the results and calculate mean and std of results
     results = torch.cat(results, dim=1)
-    for i in range(8):
-        print(torch.mean(results[i]), torch.std(results[i]))
+    # Get mean and std in [[mean, std], [mean, std], ...] format
+    results = torch.stack([results.mean(dim=1), results.std(dim=1)], dim=1)
+    # Convert to [mean, std, mean, std]
+    results = results.flatten().tolist()
+    # Add sample rate to the beginning of the list
+    results.insert(0, args.sr)
+
+    if args.resume_from == 629:
+        save_results_to_csv(results, "results_48kHz.csv")
+    elif args.resume_from == 584:
+        save_results_to_csv(results, "results_16kHz.csv")
+    else:
+        save_results_to_csv(results)
 
 
 if __name__ == "__main__":
@@ -163,6 +236,9 @@ if __name__ == "__main__":
         help="Start from ema checkpoint",
     )
     parser.add_argument("--save", action="store_true", required=False, help="Save file")
+    parser.add_argument(
+        "--cuda", action="store_true", required=True, help="Enable CPU running"
+    )
     parser.add_argument("--sr", type=int, required=True, help="input sampling rate")
 
     args = parser.parse_args()
